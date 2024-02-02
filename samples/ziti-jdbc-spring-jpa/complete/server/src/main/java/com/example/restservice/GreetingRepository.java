7a7ef0fca34ac8fb7aa40a5ebe6a0888f5adfc1c
zhaojun <zhaojun@mingyangtech.com.cn>
2024-02-02 16:13:21 +0800

    1.0.2test

--- samples/ziti-jdbc-spring-jpa/complete/server/src/main/java/com/example/restservice/GreetingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.restservice;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "greetings", path = "greetings")
-public interface GreetingRepository extends PagingAndSortingRepository<Greeting, Long> {
-
-}
--- samples/ziti-jdbc-spring-jpa/network/teardown.sh
+++
@@ -1,1 +0,0 @@
-docker-compose -p zjpa -f simplified-docker-compose.yml -f docker-compose.yml down -v
